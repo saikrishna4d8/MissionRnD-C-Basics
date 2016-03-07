b30a39802a614fdcd0175903735aa3c4b37f0360
saikrishna4d8 <saikrishna4d8@missionrnd.com>
2016-03-07 10:10:46 -0800

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -14,10 +14,10 @@ NOTES:
 #include<stdio.h>
 #include<conio.h>
 int count(int num1, int num2) {
-	if (num1 > num2)
+	if (num1==0)
 		return -1;
-	else if (num1 == 0)
-		return -1;
+	else if (num1>num2)
+		return 0;
 	else if (num1 == num2)
 		return 1;
 	else
